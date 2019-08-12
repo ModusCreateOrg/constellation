331ef6d47af4ba928d6f27156f785cb1aebe2b0d
Richard Bullington-McGuire <richard@moduscreate.com>
2019-08-12 17:31:58 -0400

    Pin versioin of eks terraform to v4.0.2
    Newer versions require Terraform 0.12+, but v4.0.2 is compatible with
    Terraform 0.11

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,5 +1,5 @@
 module "demo-cluster" {
-  source       = "terraform-aws-modules/eks/aws"
+  source       = "github.com/terraform-aws-modules/terraform-aws-eks?ref=v4.0.2"
   cluster_name = "${var.project_name}-cluster"
   subnets      = ["${module.vpc.public_subnets}"]
   vpc_id       = "${module.vpc.vpc_id}"
