0f5714c1da2fa130927e47b39a622d2b738b178d
Richard Bullington-McGuire <richard@moduscreate.com>
2019-08-12 17:36:42 -0400

    Try to pin version of both eks and vpc modules

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,5 +1,6 @@
 module "demo-cluster" {
-  source       = "github.com/terraform-aws-modules/terraform-aws-eks?ref=v4.0.2"
+  source       = "terraform-aws-modules/terraform-aws-eks"
+  version      = "~> v4.0.2"
   cluster_name = "${var.project_name}-cluster"
   subnets      = ["${module.vpc.public_subnets}"]
   vpc_id       = "${module.vpc.vpc_id}"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source = "github.com/terraform-aws-modules/terraform-aws-vpc"
+  version = "~> v1.50.0"
 
   name = "${var.project_name}-vpc"
 
