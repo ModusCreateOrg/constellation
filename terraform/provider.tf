0a2898fde43ad7622006c61085fc2e3165ce660a
Richard Bullington-McGuire <richard@moduscreate.com>
2022-12-13 15:55:28 -0500

    Separate sections better

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  version = "~> 2.23"
+}
+
+provider "local" {
+  version = "~> 1.3"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
